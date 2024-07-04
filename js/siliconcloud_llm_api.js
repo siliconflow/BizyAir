c31a7941d2e8c0eec3d3afc52cdb70593c0dd351
Yao Chi <later@usopp.net>
2024-07-04 14:02:46 +0800

    add show text feature for llm node

---
+++ js/siliconcloud_llm_api.js
@@ -0,0 +1,48 @@
+import { app } from "../../scripts/app.js";
+import { ComfyWidgets } from "../../../scripts/widgets.js";
+
+app.registerExtension({
+    name: "comfyair.siliconcloud.llm.api",
+    async beforeRegisterNodeDef(nodeType, nodeData, app) {
+        if (nodeData.name === "ComfyAirSiliconCloudLLMAPI") {
+            function populate(text) {
+                if (this.widgets) {
+                    const pos = this.widgets.findIndex((w) => w.name === "showtext");
+                    if (pos !== -1) {
+                        for (let i = pos; i < this.widgets.length; i++) {
+                            this.widgets[i].onRemove?.();
+                        }
+                        this.widgets.length = pos;
+                    }
+                }
+
+                for (const list of text) {
+                    const w = ComfyWidgets["STRING"](this, "showtext", ["STRING", { multiline: true }], app).widget;
+                    w.inputEl.readOnly = true;
+                    w.inputEl.style.opacity = 0.6;
+                    w.value = list;
+                }
+
+                requestAnimationFrame(() => {
+                    const sz = this.computeSize();
+                    if (sz[0] < this.size[0]) {
+                        sz[0] = this.size[0];
+                    }
+                    if (sz[1] < this.size[1]) {
+                        sz[1] = this.size[1];
+                    }
+                    this.onResize?.(sz);
+                    app.graph.setDirtyCanvas(true, false);
+                });
+            }
+
+            const onExecuted = nodeType.prototype.onExecuted;
+            nodeType.prototype.onExecuted = function (message) {
+                // message is the ui_obj return by python backend {"ui": ui_obj, "result": result_obj}
+                onExecuted?.apply(this, arguments);
+                // python backend return {"ui": {"text": text}, ...}, so message has the attribute text
+                populate.call(this, message.text);
+            };
+        }
+    },
+})
--- llm.py
+++ llm.py
@@ -46,6 +46,7 @@ class SiliconCloudLLMAPI:
 
     RETURN_TYPES = ("STRING",)
     FUNCTION = "get_llm_model_response"
+    OUTPUT_NODE = True
 
     CATEGORY = "ComfyAir"
 
@@ -61,39 +62,11 @@ class SiliconCloudLLMAPI:
         )
         ret = json.loads(response.text)
         text = ret["choices"][0]["message"]["content"]
-        return (text,)
-
-
-class ShowText:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {"text": ("STRING", {"forceInput": True}),},
-            "hidden": {"unique_id": "UNIQUE_ID", "extra_pnginfo": "EXTRA_PNGINFO",},
-        }
-
-    INPUT_IS_LIST = True
-    RETURN_TYPES = ("STRING",)
-    FUNCTION = "notify"
-    OUTPUT_NODE = True
-    OUTPUT_IS_LIST = (True,)
-
-    CATEGORY = "ComfyAir"
-
-    def notify(self, text, unique_id=None, extra_pnginfo=None):
-        if unique_id and extra_pnginfo and "workflow" in extra_pnginfo[0]:
-            workflow = extra_pnginfo[0]["workflow"]
-            node = next(
-                (x for x in workflow["nodes"] if str(x["id"]) == unique_id[0]), None
-            )
-            if node:
-                node["widgets_values"] = [text]
-        return {"ui": {"text": text}, "result": (text,)}
+        return {"ui": {"text": (text,)}, "result": (text,)}
 
 
 NODE_CLASS_MAPPINGS = {
     "ComfyAirSiliconCloudLLMAPI": SiliconCloudLLMAPI,
-    "ComfyAirShowText": ShowText,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ComfyAirSiliconCloudLLMAPI": "ComfyAir SiliconCloud LLM API",
