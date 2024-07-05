61240f7ceeabb6bd9a3ef69a8e74aa117a121897
Yao Chi <later@usopp.net>
2024-07-05 10:54:12 +0800

    Add siliconcloud llm node (#3)
    * refine
    
    * add generate realvis4 lightning node
    
    * fix RGBA not supported problem
    
    * refine, add if branch for local deubg/online prodcut mode
    
    * refine
    
    * add  comfyair debug flag
    
    * refine
    
    * refine, seed from 0 to 1
    
    * refine env name
    
    * refine env name
    
    * refine, validate api key
    
    * refine load api key
    
    * refine
    
    * refine
    
    * to do:add starsymbols for password and show result text on node
    
    * add show text feature for llm node
    
    * print invalid key
    
    * reset default prompt

--- __init__.py
+++ __init__.py
@@ -1,8 +1,13 @@
 from . import supernode
+from . import llm
 
 NODE_CLASS_MAPPINGS = {
     **supernode.NODE_CLASS_MAPPINGS,
+    **llm.NODE_CLASS_MAPPINGS,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     **supernode.NODE_DISPLAY_NAME_MAPPINGS,
+    **llm.NODE_DISPLAY_NAME_MAPPINGS,
 }
+
+WEB_DIRECTORY = "./js"
