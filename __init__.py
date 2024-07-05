e1701401b6951fec20cfb007cfa9aa2ca3f21002
Yao Chi <later@usopp.net>
2024-07-05 14:32:38 +0800

    refine the auth method, use the get/post and cookies to set the api key

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
+from . import auth
 from . import supernode
 from . import llm
 
 NODE_CLASS_MAPPINGS = {
+    **auth.NODE_CLASS_MAPPINGS,
     **supernode.NODE_CLASS_MAPPINGS,
     **llm.NODE_CLASS_MAPPINGS,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
+    **auth.NODE_DISPLAY_NAME_MAPPINGS,
     **supernode.NODE_DISPLAY_NAME_MAPPINGS,
     **llm.NODE_DISPLAY_NAME_MAPPINGS,
 }
---
+++ auth.py
@@ -0,0 +1,65 @@
+import uuid
+
+import server
+from aiohttp import web
+
+API_KEY = None
+
+
+@server.PromptServer.instance.routes.post("/bizyair/set_api_key")
+async def set_api_key(request):
+    global API_KEY
+    data = await request.post()
+    api_key = data.get("api_key")
+
+    try:
+        if api_key:
+            response = web.Response(text="ok")
+            response.set_cookie("api_key", api_key)
+            API_KEY = api_key
+            return response
+        else:
+            return web.Response(text="No token provided", status=400)
+    except Exception as e:
+        return web.Response(text=str(e), status=500)
+
+
+@server.PromptServer.instance.routes.get("/bizyair/get_api_key")
+async def get_api_key(request):
+    global API_KEY
+    api_key = request.cookies.get("api_key")
+    try:
+        if api_key:
+            API_KEY = api_key
+            return web.Response(text="ok")
+        else:
+            return web.Response(text="No api key found in cookie", status=404)
+    except Exception as e:
+        return web.Response(text="str(e)", status=500)
+
+
+class SetAPIKey:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"API_KEY": ("STRING", {"default": "YOUR_API_KEY"}),}}
+
+    RETURN_TYPES = ()
+    FUNCTION = "set_api_key"
+
+    CATEGORY = "ComfyAir"
+    OUTPUT_NODE = True
+
+    def set_api_key(self, API_KEY="YOUR_API_KEY"):
+        return {"ui": {"api_key": (API_KEY,)}, "result": ()}
+
+    @classmethod
+    def IS_CHANGED(s, latent):
+        return uuid.uuid4().hex
+
+
+NODE_CLASS_MAPPINGS = {
+    "ComfyAirSetAPIKey": SetAPIKey,
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ComfyAirSetAPIKey": "Set SiliconCloud API Key",
+}
