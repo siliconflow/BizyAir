1b28d366066020a589ba40a585b08fb5fb12cf30
Yao Chi <later@usopp.net>
2024-07-05 10:54:12 +0800

    Add siliconcloud llm node (#3)
    * refine
    
    * add generate realvis4 lightning node
    
    * fix RGBA not supported problem
    
    * refine, add if branch for local deubg/online prodcut mode
    
    * refine
    
    * add  comfyair debug flag
    
    * refine
    
    * refine, seed from 0 to 1
    
    * refine env name
    
    * refine env name
    
    * refine, validate api key
    
    * refine load api key
    
    * refine
    
    * refine
    
    * to do:add starsymbols for password and show result text on node
    
    * add show text feature for llm node
    
    * print invalid key
    
    * reset default prompt

--- utils.py
+++ utils.py
@@ -109,7 +109,7 @@ def format_bytes(num_bytes: int) -> str:
 def validate_api_key(key: str):
     assert key.startswith(
         "sk"
-    ), "invalid key, please refer to https://cloud.siliconflow.cn to get your API_KEY"
+    ), f"invalid key, please refer to https://cloud.siliconflow.cn to get your API_KEY, got {key}"
 
 
 def get_api_key():
@@ -121,3 +121,36 @@ def get_api_key():
 def set_api_key(key: str):
     validate_api_key(key)
     os.environ["COMFYAIR_API_KEY"] = key
+
+
+def get_llm_response(
+    model: str,
+    system_prompt: str,
+    user_prompt: str,
+    max_tokens: int = 1024,
+    temperature: float = 0.7,
+):
+    api_url = "https://api.siliconflow.cn/v1/chat/completions"
+    API_KEY = get_api_key()
+    validate_api_key(API_KEY)
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": f"Bearer {API_KEY}",
+    }
+
+    payload = {
+        "model": model,
+        "messages": [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        "max_tokens": max_tokens,
+        "temperature": temperature,
+        "top_p": 0.9,
+        "top_k": 50,
+        "stream": False,
+        "n": 1,
+    }
+    response = send_post_request(api_url, headers=headers, payload=payload)
+    return response
