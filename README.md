304954bfdbf740754e1ad3b689e59f2ef74b9627
Yao Chi <later@usopp.net>
2024-07-10 18:27:21 +0800

    add images in readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# BizyAir
+
+BizyAir is a collection of [ComfyUI](https://github.com/comfyanonymous/ComfyUI) nodes that help you overcome environmental and hardware limitations, allowing you to more easily generate high-quality content with ComfyUI.
+
+![](./docs/docs/kolors/imgs/bizyair_kolors_img2img.png)
+
+![](./docs/docs/getting-started/imgs/llm_api_node.png)
+
+## Table of Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## Features
+
+Run ComfyUI anywhere, anytime, without worrying about the environment or hardware requirements.
+
+## Installation
+
+You can install BizyAir by downloading the BizyAir repository to the custom_nodes subdirectory of ComfyUI by using git clone.
+
+```bash
+cd /path/to/ComfyUI && \
+git clone https://github.com/siliconflow/BizyAir.git
+```
+
+Then, restart ComfyUI. There are some workflow examples in the [examples](./examples) directory.
+
+
+## Usage
+
+Please see the [Quick Start](https://siliconflow.github.io/BizyAir/getting-started/quick-start.html) page to set up BizyAir.
--- supernode.py
+++ supernode.py
@@ -109,7 +109,7 @@ class GenerateLightningImage:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "prompt": ("STRING", {"multiline": True, "dynamicPrompts": True}),
+                "prompt": ("STRING", {"multiline": True, "dynamicPrompts": True, "default":"a dog"}),
                 "seed": ("INT", {"default": 1, "min": 0, "max": 0xFFFFFFFFFFFFFFFF}),
                 "width": ("INT", {"default": 1024, "min": 16, "max": 1024, "step": 8}),
                 "height": ("INT", {"default": 1024, "min": 16, "max": 1024, "step": 8}),
@@ -162,12 +162,12 @@ class GenerateLightningImage:
 
 
 NODE_CLASS_MAPPINGS = {
-    "BizyAirSuperResolution": SuperResolution,
+    #"BizyAirSuperResolution": SuperResolution,
     "BizyAirRemoveBackground": RemoveBackground,
     "BizyAirGenerateLightningImage": GenerateLightningImage,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "BizyAirSuperResolution": "BizyAir Anime Image Super Resolution",
+    #"BizyAirSuperResolution": "BizyAir Anime Image Super Resolution",
     "BizyAirRemoveBackground": "BizyAir Remove Background",
     "BizyAirGenerateLightningImage": "BizyAir Generate Image",
 }
