da0cc72a358074d8ecfc34a07a8839d411308610
FengWen <109639975+ccssu@users.noreply.github.com>
2024-07-27 00:20:39 +0800

    Dev support ksampler (#44)
    * support vae lora controlnet
    
    * Optimal dispatch
    
    * auto composition
    
    * fix ID Allocator & add batch mode
    
    * support ipa
    
    * remove batch
    
    * refine
    
    * Add examples for bizyair examples (#57)
    
    * add examples
    
    * fix & refine
    
    * refine
    
    * black
    
    * refine
    
    * refine
    
    * refine
    
    * refine send_request
    
    * add exaple for ipadater
    
    * reformat for mz nodes py
    
    ---------
    
    Co-authored-by: Yao Chi <later@usopp.net>

---
+++ data_types.py
@@ -0,0 +1,11 @@
+# https://docs.comfy.org/essentials/custom_node_datatypes#model-datatypes
+# Model datatypes
+MODEL = "BIZYAIR_MODEL"
+CLIP = "BIZYAIR_CLIP"
+VAE = "BIZYAIR_VAE"
+CONDITIONING = "BIZYAIR_CONDITIONING"
+CONTROL_NET = "BIZYAIR_CONTROL_NET"
+
+
+def is_model_datatype(datatype):
+    return datatype in [MODEL, CLIP, VAE, CONDITIONING, CONTROL_NET]
