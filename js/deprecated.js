3ac04d67340da2d2eb9aa4973229d3918a33dae2
Yao Chi <later@usopp.net>
2024-07-31 23:42:28 +0800

    refine

--- js/deprecated.js
+++ js/deprecated.js
@@ -7,6 +7,7 @@ app.registerExtension({
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
         const warning_msg = {
             "BizyAirSetAPIKey": "It will be available until 2024/08/15. Please click \"BizyAir Key\" button to set API key instead.",
+            "BizyAirMZChatGLM3TextEncode": "It will be available until 2024/08/15. Please use \"☁️BizyAir MinusZone ChatGLM3 Text Encode\" node instead."
             //"BizyAirSuperResolution": "It will be available until 2024/08/31. Please use \"☁️BizyAir Image Super Resolution\" node instead.",
         }
         if (Object.keys(warning_msg).includes(nodeData.name)) {
