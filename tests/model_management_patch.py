808707372aa4da687ce44a56bd7e4eb5b60403df
Yao Chi <later@usopp.net>
2024-08-08 12:35:36 +0800

    refine

--- tests/model_management_patch.py
+++ tests/model_management_patch.py
@@ -242,21 +242,7 @@ if DISABLE_SMART_MEMORY:
 
 
 def get_torch_device_name(device):
-    if hasattr(device, "type"):
-        if device.type == "cuda":
-            try:
-                allocator_backend = torch.cuda.get_allocator_backend()
-            except:
-                allocator_backend = ""
-            return "{} {} : {}".format(
-                device, torch.cuda.get_device_name(device), allocator_backend
-            )
-        else:
-            return "{}".format(device.type)
-    elif is_intel_xpu():
-        return "{} {}".format(device, torch.xpu.get_device_name(device))
-    else:
-        return "CUDA {}: {}".format(device, torch.cuda.get_device_name(device))
+    return "CPU"
 
 
 try:
