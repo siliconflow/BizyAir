f5280aa69da6f8abc597ae9fb9421500a32268ea
Yao Chi <later@usopp.net>
2024-08-08 12:31:25 +0800

    refine

--- tests/model_management_patch.py
+++ tests/model_management_patch.py
@@ -102,33 +102,7 @@ def get_torch_device():
 
 
 def get_total_memory(dev=None, torch_total_too=False):
-    global directml_enabled
-    if dev is None:
-        dev = get_torch_device()
-
-    if hasattr(dev, "type") and (dev.type == "cpu" or dev.type == "mps"):
-        mem_total = psutil.virtual_memory().total
-        mem_total_torch = mem_total
-    else:
-        if directml_enabled:
-            mem_total = 1024 * 1024 * 1024  # TODO
-            mem_total_torch = mem_total
-        elif is_intel_xpu():
-            stats = torch.xpu.memory_stats(dev)
-            mem_reserved = stats["reserved_bytes.all.current"]
-            mem_total_torch = mem_reserved
-            mem_total = torch.xpu.get_device_properties(dev).total_memory
-        else:
-            stats = torch.cuda.memory_stats(dev)
-            mem_reserved = stats["reserved_bytes.all.current"]
-            _, mem_total_cuda = torch.cuda.mem_get_info(dev)
-            mem_total_torch = mem_reserved
-            mem_total = mem_total_cuda
-
-    if torch_total_too:
-        return (mem_total, mem_total_torch)
-    else:
-        return mem_total
+    return 20 * 1024 * 1024 * 1024
 
 
 total_vram = get_total_memory(get_torch_device()) / (1024 * 1024)
