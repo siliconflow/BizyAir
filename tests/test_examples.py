6491e7570bb1430bd5429d128f89cbe611855459
Yao Chi <later@usopp.net>
2024-08-08 14:22:36 +0800

    refine

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,32 +2,51 @@
 
 import os
 import time
-
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-COMFY_HOST = os.getenv("COMFY_HOST", "127.0.0.1")
-COMFY_PORT = os.getenv("COMFY_PORT", "8188")
-BIZYAIR_KEY = os.getenv("BIZYAIR_KEY", "")
 
-base_path = os.path.dirname(os.path.abspath(__file__))
-print(f"Test base path: {base_path}")
+def wait_for_comfy_ready(host="127.0.0.1", port=8188, wait_time_secs=120):
+    url = f"http://{host}:{port}/system_stats"
+    wait_secs = wait_time_secs
+
+    while wait_secs > 0:
+        print(f"Waiting for ComfyUI server ready at {url}, {wait_secs}s left ...")
+        time.sleep(1)
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                return
+        except requests.RequestException:
+            pass
+
+        wait_secs -= 1
+
+    print(f"Timeout {wait_time_secs} secs. ComfyUI Server not ready.")
+    exit(1)
 
 
-# Wait for the page to load
-print(f"wainting for comfyui to start")
-time.sleep(10)
-print(f"End wainting for comfyui to start")
+if __name__ == "__main__":
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+    COMFY_HOST = os.getenv("COMFY_HOST", "127.0.0.1")
+    COMFY_PORT = os.getenv("COMFY_PORT", "8188")
+    BIZYAIR_KEY = os.getenv("BIZYAIR_KEY", "")
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(f"http://{COMFY_HOST}:{COMFY_PORT}")
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    print(f"Test base path: {base_path}")
 
-# Add a cookie
-driver.add_cookie({"name": "api_key", "value": BIZYAIR_KEY, "path": "/"})
+    wait_for_comfy_ready(host=COMFY_HOST, port=COMFY_PORT, wait_time_secs=120)
 
-driver.quit()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(f"http://{COMFY_HOST}:{COMFY_PORT}")
+
+    # Add a cookie
+    driver.add_cookie({"name": "api_key", "value": BIZYAIR_KEY, "path": "/"})
+
+    driver.quit()
