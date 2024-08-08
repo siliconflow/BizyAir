0a44873e33bc3f3314fe643868321bbe2e0b99f6
Yao Chi <later@usopp.net>
2024-08-08 10:30:16 +0800

    refine

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -10,6 +10,9 @@ COMFY_HOST = os.getenv("COMFY_HOST", "127.0.0.1")
 COMFY_PORT = os.getenv("COMFY_PORT", "8188")
 BIZYAIR_KEY = os.getenv("BIZYAIR_KEY", "")
 
+base_path = os.path.dirname(os.path.abspath(__file__))
+comfy_path = os.path.join(base_path, "comfyui")
+
 
 def test_comfyui():
     chrome_options = Options()
