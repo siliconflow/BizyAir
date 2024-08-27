88157796e54577d004b453248ea64094bf650bd9
FengWen <109639975+ccssu@users.noreply.github.com>
2024-08-27 18:35:52 +0800

    Refactor Bizyair and Code Formatting Improvements (#104)
    * Refactor Bizyair and Code Formatting Improvements
    
    * refine
    
    * Delete redundant code
    
    * Delete redundant code
    
    * fix UNIQUE_ID
    
    * refine
    
    * refine
    
    * refine
    
    * skip Super Resolution test
    
    * Add lora test server (#105)
    
    * refine
    
    * Delete src/bizyair/commands/servers/server_base.py
    
    ---------
    
    Co-authored-by: Yao Chi <later@usopp.net>

--- bizyair_extras/nodes_ipadapter_plus/nodes_ipadapter_plus.py
+++ bizyair_extras/nodes_ipadapter_plus/nodes_ipadapter_plus.py
@@ -4,9 +4,8 @@ import os
 import folder_paths
 import torch
 
-from bizyair import BizyAirBaseNode
+from bizyair import BizyAirBaseNode, BizyAirNodeIO, create_node_data
 from bizyair.data_types import CLIP, CONDITIONING, MODEL
-from bizyair.image_utils import BizyAirNodeIO, create_node_data
 
 # set the models directory
 if "ipadapter" not in folder_paths.folder_names_and_paths:
