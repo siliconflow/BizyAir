efc6ab82ab43a4b3f5db78dcb89d8d7f99c63977
dantegarden <761043617@qq.com>
2024-09-05 11:31:04 +0800

    update: adjust structure

---
+++ src/bizy_server/resp.py
@@ -0,0 +1,19 @@
+from .errno import ErrorNo, CODE_OK
+from aiohttp import web
+
+
+def JsonResponse(http_status_code, data):
+    return web.json_response(
+        data, status=http_status_code, content_type="application/json",
+    )
+
+
+def OKResponse(data):
+    return JsonResponse(200, {"message": "success", "code": CODE_OK, "data": data})
+
+
+def ErrResponse(err: ErrorNo):
+    return JsonResponse(
+        err.http_status_code,
+        {"message": err.message, "code": err.code, "data": err.data},
+    )
