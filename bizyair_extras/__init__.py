7bec5655fd047736ad1148f1f55c155f2b6f9321
FengWen <ccsuwen@gmail.com>
2024-09-23 15:28:16 +0800

    Support lora training POC

--- bizyair_extras/__init__.py
+++ bizyair_extras/__init__.py
@@ -1,5 +1,6 @@
 from .nodes_comfyui_instantid import *
 from .nodes_controlnet import *
+from .nodes_flux_trainer import *
 from .nodes_image_utils import *
 from .nodes_ipadapter_plus.nodes_ipadapter_plus import *
 from .nodes_kolors_mz import *
