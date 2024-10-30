c5ad36341305aec60097b11ece9d94bd3296173d
of-HaiQin <5372604@qq.com>
2024-10-30 16:03:06 +0800

    [feat]optimize tailwind config

--- biz_lib_frontend/vite.config.ts
+++ biz_lib_frontend/vite.config.ts
@@ -3,6 +3,7 @@ import tailwind from 'tailwindcss'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
   define: {
@@ -14,7 +15,7 @@ export default defineConfig({
     },
   },
   plugins: [
-    vue(),
+    vue(),cssInjectedByJsPlugin()
   ],
   resolve: {
     alias: {
