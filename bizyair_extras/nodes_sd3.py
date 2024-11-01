aa5b8fbf42c83e3427bb46b835181acf17e09812
FengWen <ccsuwen@gmail.com>
2024-11-01 09:12:37 +0800

    support fluxcontrolnet

--- bizyair_extras/nodes_sd3.py
+++ bizyair_extras/nodes_sd3.py
@@ -18,3 +18,35 @@ class TripleCLIPLoader(BizyAirBaseNode):
     # FUNCTION = "load_clip"
 
     CATEGORY = "advanced/loaders"
+
+
+class ControlNetApplySD3(BizyAirBaseNode):
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "positive": (data_types.CONDITIONING,),
+                "negative": (data_types.CONDITIONING,),
+                "control_net": (data_types.CONTROL_NET,),
+                "vae": (data_types.VAE,),
+                "image": ("IMAGE",),
+                "strength": (
+                    "FLOAT",
+                    {"default": 1.0, "min": 0.0, "max": 10.0, "step": 0.01},
+                ),
+                "start_percent": (
+                    "FLOAT",
+                    {"default": 0.0, "min": 0.0, "max": 1.0, "step": 0.001},
+                ),
+                "end_percent": (
+                    "FLOAT",
+                    {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.001},
+                ),
+            }
+        }
+
+    CATEGORY = "conditioning/controlnet"
+    # DEPRECATED = True
+    NODE_DISPLAY_NAME = "Apply Controlnet with VAE"
+    RETURN_TYPES = (data_types.CONDITIONING, data_types.CONDITIONING)
+    RETURN_NAMES = ("positive", "negative")
