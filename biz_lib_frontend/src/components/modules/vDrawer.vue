417d0e12a605182a4270550eadc0c2e11b9a18e6
linjm8780860 <lin8780860@163.com>
2024-11-18 15:11:45 +0800

    fix

--- biz_lib_frontend/src/components/modules/vDrawer.vue
+++ biz_lib_frontend/src/components/modules/vDrawer.vue
@@ -16,7 +16,7 @@ const close = () => {
 </script>
 <template>
   <Teleport to="body">
-    <div class="w-[100vw] h-[100vh] fixed inset-0 z-50 bg-black/80 " v-if="open">
+    <div class="w-[100vw] h-[100vh] fixed inset-0 z-8000 bg-black/80 " v-if="open">
       <div :class="cn('fixed z-50 bg-background p-6 shadow-lg h-full border-r w-auto min-w-[560px]', props.class)">
         <X class="absolute right-2 top-2 cursor-pointer" @click="close" />
         <slot />
