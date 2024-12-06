b0e7f892e731ea3621f8b1ec2697172f0f7f49ba
of-HaiQin <5372604@qq.com>
2024-12-06 14:47:40 +0800

    [feat]fix type name

--- js/model_apply.js
+++ js/model_apply.js
@@ -155,7 +155,7 @@ app.registerExtension({
             nodeType.prototype.onNodeCreated = function() {
                 try {
                     const result = onNodeCreated?.apply(this, arguments);
-                    createSetWidgetCallback("ControlNet").call(this);
+                    createSetWidgetCallback("Controlnet").call(this);
                     return result;
                 } catch (error) {
                     console.error("Error in node creation:", error);
@@ -166,7 +166,7 @@ app.registerExtension({
 
     async nodeCreated(node) {
         if (node?.comfyClass === "BizyAir_ControlNetLoader") {
-            setupNodeMouseBehavior(node, "ControlNet");
+            setupNodeMouseBehavior(node, "Controlnet");
         }
     }
 })
