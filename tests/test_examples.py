49f8c8c045d9a9111f75b8548d6084c276c96b51
zhujintao <2030442731@qq.com>
2024-12-11 16:49:18 +0800

    decrease test step to 1

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -31,7 +31,28 @@ def wait_for_comfy_ready(host="127.0.0.1", port=8188, wait_time_secs=120):
     print(f"Timeout {wait_time_secs} secs. ComfyUI Server not ready.")
     exit(1)
 
+def modify_steps_decorator(func):
+    def wrapper(*args, **kwargs):
+        json_content = func(*args, **kwargs)
+        
+        try:
+            data = json.loads(json_content)
+        except json.JSONDecodeError as e:
+            raise ValueError(f"Invalid JSON content: {e}")
+        
+        for node in data['nodes']:
+            if node['type'] == 'BizyAir_BasicScheduler':
+                if len(node['widgets_values']) == 3:
+                    node['widgets_values'][1] = 1
+                else:
+                    raise ValueError("BizyAir_BasicScheduler widget values is wrong")
+        
+        modified_json_content = json.dumps(data, indent=2)
+        return modified_json_content
+    
+    return wrapper
 
+@modify_steps_decorator
 def read_workflow_json(filename) -> str:
     _, extension = os.path.splitext(filename)
     if extension.endswith("json"):
