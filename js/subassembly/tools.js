578b362c02bab96edb7b9fda7242c6037cb9000e
of-HaiQin <5372604@qq.com>
2024-12-12 11:26:28 +0800

    [fix]

--- js/subassembly/tools.js
+++ js/subassembly/tools.js
@@ -11,7 +11,6 @@ export const hideWidget = (node, widget_name) => {
   widget.height = 0;
   widget.type = "hidden";
   widget.options = widget.options || {};
-
   widget.show = () => {
     widget.computeSize = originalComputeSize;
     widget.type = originalType;
