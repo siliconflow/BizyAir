3099a61afd6ef32567af4b0c20e698942e58ed30
FengWen <ccsuwen@gmail.com>
2024-12-14 07:29:58 +0800

    refine

--- src/bizyair/common/client.py
+++ src/bizyair/common/client.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import pprint
 import time
--- src/bizyair/common/task_manager/task_runner.py
+++
@@ -1,9 +0,0 @@
-from .task_base import TaskContextBase, TaskRunnerBase
-
-
-class BizyAirTaskRunner(TaskRunnerBase):
-    def __init__(self, task_context: TaskContextBase):
-        self.task_context = task_context
-
-    def execute(self, task_id: str):
-        pass
