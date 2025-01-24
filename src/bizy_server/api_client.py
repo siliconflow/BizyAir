80e3061aa7bba84eb7b53c3ac7035ed797e3bce8
YaoChi <later@usopp.net>
2025-01-24 18:08:29 +0800

    fromat

--- src/bizy_server/api_client.py
+++ src/bizy_server/api_client.py
@@ -360,7 +360,9 @@ class APIClient:
             print(f"\033[31m[BizyAir]\033[0m Fail to fork model version: {str(e)}")
             return None, errnos.FORK_MODEL_VERSION
 
-    async def unfork_model_version(self, version_id: int) -> tuple[None, ErrorNo | None]:
+    async def unfork_model_version(
+        self, version_id: int
+    ) -> tuple[None, ErrorNo | None]:
         server_url = f"{BIZYAIR_SERVER_ADDRESS}/bizy_models/versions/{version_id}/fork"
 
         headers, err = self.auth_header()
@@ -376,7 +378,7 @@ class APIClient:
         except Exception as e:
             print(f"\033[31m[BizyAir]\033[0m Fail to unfork model version: {str(e)}")
             return None, errnos.UNFORK_MODEL_VERSION
-    
+
     async def update_model(
         self, model_id: int, name: str, type_: str, versions: list[dict]
     ) -> tuple[dict | None, ErrorNo | None]:
