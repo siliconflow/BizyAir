709be1b1e5962bf699e4384c351e22cb665d9c53
wangshier108 <chihiro_why@163.com>
2025-02-10 11:35:42 +0800

    add mochi nodes

---
+++ bizyair_extras/nodes_mochi.py
@@ -0,0 +1,22 @@
+from bizyair import BizyAirBaseNode
+
+class BizyAir_mochi(BizyAirBaseNode):
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "prompt": ("STRING", {"default": "Close-up of a chameleon's eye, with its scaly skin changing color. Ultra high resolution 4k."}),
+                "steps": ("INT", {"default": 20, "min": 1, "max": 50}),
+                "width": ("INT", {"default": 64, "min": 64, "max": 1560, "step": 8}),
+                "height": ("INT", {"default": 64, "min": 64, "max": 1560, "step": 8}),
+                "frames": ("INT", {"default": 96, "min": 1, "max": 4096}),
+                "fps": ("INT", {"default": 22, "min": 8, "max": 64}),
+                              
+            }
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("path",)
+    CATEGORY = "☁️BizyAir/Trellis"
+    NODE_DISPLAY_NAME = "☁️BizyAir mochi"
+    OUTPUT_NODE = True
