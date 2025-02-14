126b7e915076a9b0c27f7369101ad2fc571fdea6
wangshier108 <chihiro_why@163.com>
2025-02-14 11:00:23 +0800

    refine the  code

--- bizyair_extras/nodes_mochi.py
+++ bizyair_extras/nodes_mochi.py
@@ -7,8 +7,8 @@ class BizyAir_mochi(BizyAirBaseNode):
             "required": {
                 "prompt": ("STRING", {"default": "Close-up of a chameleon's eye, with its scaly skin changing color. Ultra high resolution 4k."}),
                 "steps": ("INT", {"default": 20, "min": 1, "max": 50}),
-                "width": ("INT", {"default": 64, "min": 64, "max": 1560, "step": 8}),
-                "height": ("INT", {"default": 64, "min": 64, "max": 1560, "step": 8}),
+                "width": ("INT", {"default": 480, "min": 64, "max": 1536, "step": 8}),
+                "height": ("INT", {"default": 848, "min": 64, "max": 1536, "step": 8}),
                 "frames": ("INT", {"default": 96, "min": 1, "max": 4096}),
                 "fps": ("INT", {"default": 22, "min": 8, "max": 64}),
                 "filename": ("STRING", {"default": "mochi"}),                    
