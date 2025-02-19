ea7e5712ba9f7f7a1633e68e8806673f749db323
wangshier108 <chihiro_why@163.com>
2025-02-19 10:52:41 +0800

    modify output type

--- bizyair_extras/nodes_mochi.py
+++ bizyair_extras/nodes_mochi.py
@@ -7,16 +7,16 @@ class BizyAir_mochi(BizyAirBaseNode):
             "required": {
                 "prompt": ("STRING", {"default": "Close-up of a chameleon's eye, with its scaly skin changing color. Ultra high resolution 4k."}),
                 "steps": ("INT", {"default": 20, "min": 1, "max": 50}),
-                "width": ("INT", {"default": 480, "min": 64, "max": 1536, "step": 8}),
-                "height": ("INT", {"default": 848, "min": 64, "max": 1536, "step": 8}),
+                "width": ("INT", {"default": 848, "min": 64, "max": 1536, "step": 8}),
+                "height": ("INT", {"default": 480, "min": 64, "max": 1536, "step": 8}),
                 "frames": ("INT", {"default": 96, "min": 1, "max": 4096}),
                 "fps": ("INT", {"default": 22, "min": 8, "max": 64}),
                 "filename": ("STRING", {"default": "mochi"}),                    
             }
         }
 
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("path",)
-    CATEGORY = "☁️BizyAir/Trellis"
+    RETURN_TYPES = ("IMAGE",)
+    # RETURN_NAMES = ("path",)
+    CATEGORY = "☁️BizyAir/mochi"
     NODE_DISPLAY_NAME = "☁️BizyAir mochi"
     OUTPUT_NODE = True
