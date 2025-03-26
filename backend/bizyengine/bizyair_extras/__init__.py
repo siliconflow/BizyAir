c982556a979e41e1a3dea16d64c99949c6c0b60c
wangshier108 <chihiro_why@163.com>
2025-03-26 15:59:38 +0800

    add rf inversion nodes

--- backend/bizyengine/bizyair_extras/__init__.py
+++ backend/bizyengine/bizyair_extras/__init__.py
@@ -15,6 +15,7 @@ from .nodes_ipadapter_plus.nodes_ipadapter_plus import *
 from .nodes_janus_pro import *
 from .nodes_kolors_mz import *
 from .nodes_model_advanced import *
+from .nodes_rf_inversion import *
 from .nodes_sd3 import *
 from .nodes_segment_anything import *
 from .nodes_testing_utils import *
